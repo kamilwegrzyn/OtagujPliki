3f176bff6eeaf52303c25802e1857de5df7d86e0
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2019-11-12 23:12:01 +0100

    Testy konstruktora

--- src/Tests/OtagujPlikiTests/OtagujPlikiTests.cs
+++ src/Tests/OtagujPlikiTests/OtagujPlikiTests.cs
@@ -7,12 +7,14 @@ namespace OtagujPlikiTests
     [TestClass]
     public class OtagujPlikiTests
     {
-        [DataRow(@"C:\Users\User\Desktop")]
+        #region Test Konstruktora
+        [DataRow(@"C:\\Users\User\Desktop", "*.txt")]
         [TestMethod]
-        public void Test_Konstruktora_Poprawny_Argument(string path)
+        public void Test_Konstruktora_Poprawny_Argument(string path, string type)
         {
-            Searcher searcher = new Searcher(path);
+            Searcher searcher = new Searcher(path, type);
             Assert.AreEqual(searcher.Path, path);
+            Assert.AreEqual(searcher.Type, type);
         }
 
         [DataRow(1)]
@@ -22,5 +24,6 @@ namespace OtagujPlikiTests
         {
             Searcher searcher = new Searcher(path);
         }
+        #endregion
     }
 }
