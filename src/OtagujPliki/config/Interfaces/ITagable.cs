4c03afe1570abc145318cf609f0e7d16beff6062
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2020-01-08 23:43:12 +0100

    Aktualizacja interfejsu i funkcji - dodano referencje SQLite do projektu

--- src/OtagujPliki/config/Interfaces/ITagable.cs
+++ src/OtagujPliki/config/Interfaces/ITagable.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using config.Interfaces;
 
-namespace config.libs
+namespace config.Interfaces
 {
-    public class SaveTags : ITagable
+    public interface ITagable
     {
     }
 }
